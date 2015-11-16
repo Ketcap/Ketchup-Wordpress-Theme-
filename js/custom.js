9bc015b6dee1722a4b275a109caa1b58f64d12e7
Uğur Oruç <uoruc5@hotmail.com>
2015-11-16 21:25:03 +0200

    Items Added

---
+++ js/custom.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+  $(".mobile-menu").sideNav();
+  $(".mobile-sidebar").sideNav({edge:'right'});
+  $('.dropdown-button').dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      constrain_width: true, // Does not change width of dropdown to that of the activator
+      hover: false, // Activate on hover
+      gutter: 0, // Spacing from edge
+      belowOrigin: true, // Displays dropdown below the button
+    }
+  );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}); //Document Ready Ends
